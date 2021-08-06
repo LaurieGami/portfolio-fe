a46b62c91f5b9f1d6599af862498706d589e43ab
LaurieGami <laurievilleton@gmail.com>
2021-08-05 17:00:14 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// Components
+import NavBar from './components/NavBar/NavBar';
+
+// Pages
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </BrowserRouter>
   );
 }
 
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import './HomePage.scss';
+
+function HomePage() {
+    return (
+        <article>
+            <h1>Hi, I'm Laurie.</h1>
+            <p>I am a full-stack web developer, currently based in Kamloops, BC. My toolbox includes React, JavaScript, HTML, CSS, Sass, Node, Express & MySQL. I am passionate about action sports and the outdoors.</p>
+            <button>Learn More</button>
+        </article>
+    );
+}
+
+export default HomePage;
