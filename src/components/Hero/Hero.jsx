6582795a2f61b13b680e6aac9b79743960ed5cdf
lauriegami <laurievilleton@gmail.com>
2022-08-16 11:22:20 -0600

    Updated current location, skillsets and installed prettier

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,8 @@ import './Hero.scss';
 function Hero() {
     return (
         <section className="hero">
-            <h3 className="hero__quote">I'm a fullstack web developer based out of Kamloops, BC.</h3>
+            <h3 className="hero__quote">I am a full-stack software developer who's passionate about the outdoors and action sports 👩‍💻🌿.</h3>
+            <h5 className="hero__location">📍 Currently based in Cranbrook, BC.</h5>
             <div className="hero__buttons">
                 <a href="#projects"><button className="hero__btn">See Projects</button></a>
             </div>
